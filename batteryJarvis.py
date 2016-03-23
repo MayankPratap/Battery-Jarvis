07c219d195087d5043ba3f498b2e43324971cf18
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-03-23 16:39:45 +0530

    Added code to detect whether system is currently being charged.

--- batteryJarvis.py
+++ batteryJarvis.py
@@ -2,23 +2,30 @@ import os,time
 
 charge_now_file="/sys/class/power_supply/BAT0/charge_now"
 charge_full_file="/sys/class/power_supply/BAT0/charge_full"
+charge_status_file="/sys/class/power_supply/BAT0/status"
 
 charge_now=open(charge_now_file)
 charge_full=open(charge_full_file)
+charge_status=open(charge_status_file)
 
 charge_now_content=charge_now.read()
 charge_full_content=charge_full.read()
+charge_status_content=charge_status.read()
 
 num=int(charge_now_content)*1.0
 den=int(charge_full_content)*1.0
 
 res=num/den*100
 
-time.sleep(2)
+if res<=15 and charge_status_content!="Charging":
+    os.system("espeak 'Hey,Mayank please charge me.'")
 
-if res<=15:
-    os.system("espeak 'Hey,Mayank please charge me'")
-    time.sleep(2)
-    
-    
-    
+elif res==100 and charge_status_content=="Charging":
+    os.system("espeak 'Please dont overcharge me.'")
+
+
+   
+
+
+
+
