a542f230b0294a6401c63fb6df7077472552b113
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-03-25 11:04:50 +0530

    Update batteryJarvis.py

--- batteryJarvis.py
+++ batteryJarvis.py
@@ -1,6 +1,9 @@
+
 import os,time
 import subprocess
 import getpass
+
+from subprocess import call
 
 charge_now_file="/sys/class/power_supply/BAT0/charge_now"
 charge_full_file="/sys/class/power_supply/BAT0/charge_full"
@@ -14,24 +17,30 @@ charge_now_content=charge_now.read()
 charge_full_content=charge_full.read()
 charge_status_content=charge_status.read()
 
+#print charge_now_content
+#print charge_status_content
+
 num=int(charge_now_content)*1.0
 den=int(charge_full_content)*1.0
 
 res=num/den*100
 
+#print res
+
+
 user=getpass.getuser()
+
+#print "hello"
+
+#print user
+
+#speech="hey " + user + " please charge me."
+#call(["espeak",speech])
 
 if res<=15 and charge_status_content!="Charging":
     speech ="hey "+user+" please charge me."
-	call(["espeak",speech])
+    call(["espeak",speech])
 
 elif res==100 and charge_status_content!="Discharging":
     speech ="hey "+user+" Please dont overcharge me."
-	call(["espeak",speech])
-
-
-   
-
-
-
-
+    call(["espeak",speech])
